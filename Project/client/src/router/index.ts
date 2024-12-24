a65df1496532b4be760450b7bf9cf2827bed24d5
DanDing <1796448871@qq.com>
2024-12-24 15:25:42 +0800

    基础组件,路由导入

---
+++ Project/client/src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';//使用createWebHistory,不用内存用浏览器存储
+import HomeView from '../views/HomeView.vue';
+import CurrencyExchangeView from '../views/CurrencyExchangeView.vue';
+import NewsView from '../views/NewsView.vue';
+import NewsDetailView from '../views/NewsDetailView.vue';
+import Login from '../components/Login.vue';
+import Register from '../components/Register.vue';
+import type { RouteRecordRaw } from 'vue-router'; // 新的写法？
+
+
+const routes: RouteRecordRaw[] = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/exchange', name: 'CurrencyExchange', component: CurrencyExchangeView },
+  { path: '/news', name: 'News', component: NewsView },
+  { path: '/news/:id', name: 'NewsDetail', component: NewsDetailView },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/register', name: 'Register', component: Register },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// 不导出的话,router显示未使用
+export default router;
