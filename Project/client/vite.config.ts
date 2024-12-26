3711764313ca6a5c785a2ec35dbfc9f31b078c96
DanDing <1796448871@qq.com>
2024-12-26 17:37:21 +0800

    暂无打包

--- Project/client/vite.config.ts
+++ Project/client/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 想打包似乎说是修改这个
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue()]
 })
